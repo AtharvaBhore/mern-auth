1c980a11ce577c1b1fa1500d5f9d8e5be27883c8
AtharvaBhore <atharvabhore6@gmail.com>
2024-06-27 16:05:47 +0530

    Create sign in api route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,8 @@
 import * as db from "../db/index.js"
 import { v4 as uuidv4 } from 'uuid';
 import bcryptjs from "bcryptjs/dist/bcrypt.js";
+import {errorHandler} from "../utils/error.js"
+import jwt from "jsonwebtoken"
 
 const signup = async (req,res,next) => {
 	const {email,password,username} = req.body;
@@ -21,4 +23,32 @@ const signup = async (req,res,next) => {
 	}
 }
 
-export {signup}
+const signin = async (req,res,next) => {
+
+	const {email,password} = req.body;
+	const text = "SELECT * FROM users WHERE email = $1;";
+	const values = [
+		email
+	]
+	try {
+		const {rows} =  await db.query(text,values);
+		if(!rows[0]){
+			return next(errorHandler(404,'Invalid credentials'))
+		}
+		const validPassword = bcryptjs.compareSync(password,rows[0].password)
+		if(!validPassword){
+			return next(errorHandler(401,'Invalid credentials'));
+		}
+		//Using JSON web tokens to create token to stay signed in
+		const token = jwt.sign({id:rows[0].id},process.env.JWT_SECRET);
+		//To avoid sending hashed Password to client side
+		const {password:hashedPassword,...rest} = rows[0]
+		const expiryDate = new Date(Date.now()+3600000)
+		res.cookie('access_token',token,{httpOnly:true,expires:expiryDate}).status(200).json(rest);
+	} catch (error) {
+		next(error);
+	}
+ 
+}
+
+export {signup,signin}
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
-import {signup} from "../controllers/authController.js"
+import {signup,signin} from "../controllers/authController.js"
 import express from "express"
 
 const router = express.Router()
 
 router.post("/signup", signup)
+router.post("/signin", signin)
 
 export {router as authRoutes}
